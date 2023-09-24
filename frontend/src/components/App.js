8b9dddb39dc424f001c824e723897f24d7f04a31
Артур Динеев <diartur@yandex.ru>
2023-09-24 20:59:06 +0300

    ready for review

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -77,7 +77,7 @@ function App() {
         .then((res) => {
           if (res) {
             setLoggedIn(true);
-            setUserEmail(res.data.email);
+            setUserEmail(res.email);
             navigate("/", { replace: true })
           }
         })
