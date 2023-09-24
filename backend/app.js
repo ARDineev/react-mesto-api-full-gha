4b75904c0b4d5c15d5c7ecf688ecea20361a52d6
Артур Динеев <diartur@yandex.ru>
2023-09-24 15:29:24 +0300

    kek

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const { userLogInValidator, userCreateValidator } = require('./middlewares/valid
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-err');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // в течение 15 минут проверяются/запоминаются запросы
